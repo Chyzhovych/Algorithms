5756696521b4dc31e6960fc933e7bfae98383e1e
Viktor Chyzhovych <chyzhovichviktor@gmail.com>
2021-12-24 15:50:05 +0200

    Add first and second sorting algorithms

---
+++ algo.hpp
@@ -0,0 +1,19 @@
+#ifndef _ALGO_HPP_
+#define _ALGO_HPP_
+
+
+void swap(int *val, int *val2);
+
+void bubbleSort(int *arr, int arr_size);
+
+void insertionSort(int *arr, int arr_size);
+
+void insertionSort2(int *arr, int arr_size);
+
+void printArray(int *arr, int arr_size);
+
+
+#endif
+
+
+
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "algo.hpp"
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 12, 33, 4, 8, 6, 7, 99, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Before sorting: "<< endl;
+    printArray(arr,size);
+
+    //bubbleSort(arr,size);
+
+    insertionSort2(arr,size);
+
+    cout << "After sorting: "<< endl;
+    printArray(arr,size);
+
+
+    return 0;
+}
